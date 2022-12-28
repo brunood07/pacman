384d74c77e0fb8bc74ec931eafe0e083bdf50b60
Bruno Domingues da Silva <brunood07@gmail.com>
2022-12-28 08:51:02 -0300

    feat: game finished

---
+++ pacman.h
@@ -0,0 +1,18 @@
+#ifndef _PACMAN_H_
+#define _PACMAN_H_
+
+#define CIMA 'w'
+#define BAIXO 's'
+#define DIREITA 'd'
+#define ESQUERDA 'a'
+#define BOMBA 'b'
+
+void move(char direcao);
+void fantasmas();
+int acabou();
+int ehdirecao(char direcao);
+int praondeofantasmavai(int xatual, int yatual, int* xdestino, int* ydestino);
+void explodepilula();
+void explodepilula2(int x, int y, int somax, int somay, int qtd);
+
+#endif
